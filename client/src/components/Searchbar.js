241e9aac7e66e891f23d3f11b7d9a9378235da13
Aaron Storozinszki <115890722+Storiaron@users.noreply.github.com>
2023-03-08 12:57:30 +0100

    add functionality to the favourites button

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,24 +1,52 @@
 import React from 'react'
 import { useState } from 'react'
 
+function Favourites({details}){
+    function handleClick(val){
+        console.log(val, details.name);
+        fetch('/', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({"id": val, "title": details.name}), 
+          });
+    }
+   
+
+      return (
+        <button className='favourite' value={details.id} onClick={e=>handleClick(e.currentTarget.value)}>{details.id}</button>
+      )
+}
 
 function SearchBar() {
-    const [searched, setSearched] = useState('')
-    let moviesDisplayed;
+    const [search, setsearch] = useState('')
+    const [hasSearched, setHasSearched] = useState(false)
+    const [moviesDisplayed, setMoviesDisplayed] = useState([])
+    
+    
     function handleInput(e) {
-        setSearched(e.target.value)
+        setsearch(e.target.value)
     }
     function handleClick(){
-        let url = `https://api.themoviedb.org/3/search/movie?api_key=856cfdccc26bfd6f158554be3cfbbdf7&language=en-US&query=${searched}&page=1&include_adult=false`
-        fetch(url).then(res => res.json()).then(movie => moviesDisplayed = movie)
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=856cfdccc26bfd6f158554be3cfbbdf7&language=en-US&query=${search}&page=1&include_adult=false`
+        fetch(url).then(res => res.json()).then(searchResults => {
+            setMoviesDisplayed(searchResults.results)
+            
+        })
+       
+
+        setHasSearched(true)
         }
 
-
-    return (
-        <a>
+    return hasSearched?(
+       <div>{moviesDisplayed.map((movie, i)=> <><div key={i}>{movie.title}</div> <Favourites details={{id:movie.id, name: movie.title}}/></>)}</div>
+    ):(
+        <div>
             <input onInput={(e) => handleInput(e)}></input>
             <button onClick={handleClick}>Search</button>
-        </a>
+            
+        </div>
     )
 }
 
--- server/server.js
+++ server/server.js
@@ -22,10 +22,9 @@ app.get('/', (req, res) => {
 
 app.post('/', (req,res)=>{
     console.log(req.body);
-    /* Fav.create({id: 'test', title: req.body}) */
-   /*  Fav.create({id: req.body.id, title: req.body.title, })
-    .then(movie => console.log(movie))
-    .catch(err => console.log(err)) */
+    Fav.create({id: Number(req.body.id), title: req.body.title})
+    .then(result => console.log(result))
+    .catch(err => console.log("Dis but an error:" ,err))
 })
 
 app.listen(5000, () => console.log('Server is listening on port 5000'));
