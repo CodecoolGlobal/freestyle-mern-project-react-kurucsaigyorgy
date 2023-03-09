e47eaea3503d3b325ca029c1c123491e23e3fbe5
Aaron Storozinszki <115890722+Storiaron@users.noreply.github.com>
2023-03-09 15:04:07 +0100

    changed relative path of movielogo

--- client/src/components/HeaderComponent.js
+++ client/src/components/HeaderComponent.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Menu from "../components/Menu";
-import moviefylogo from "/Users/pfviktor/codecool/freestyle-mern-project-react-kurucsaigyorgy/client/src/components/moviefylogo.png";
+import moviefylogo from "./moviefylogo.png"
 
 
 function HeaderComponent() {
   return (
     <header>
-      <div class="logo">
+      <div className="logo">
       <img id="logo" src={moviefylogo} alt="Moviefy Logo"/>
       </div>
       <Menu />
