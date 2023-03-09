3fc11da5208d29599b6b29e08920bbb8a10429e2
Aaron Storozinszki <115890722+Storiaron@users.noreply.github.com>
2023-03-09 11:09:46 +0100

    add useEffect to fetch requests

--- client/src/components/Recommendations.js
+++ client/src/components/Recommendations.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useEffect } from "react";
 
 function Recommendations() {
-fetch("https://api.themoviedb.org/3/movie/8008/recommendations?api_key=856cfdccc26bfd6f158554be3cfbbdf7&language=en-US&page=1")
+
+    useEffect(()=>{
+        fetch("https://api.themoviedb.org/3/movie/8008/recommendations?api_key=856cfdccc26bfd6f158554be3cfbbdf7&language=en-US&page=1")
 .then(res => res.json())
 .then(rec => console.log(rec))
+
+    }, [])
+
 
     return (
         <div>'saup</div>
